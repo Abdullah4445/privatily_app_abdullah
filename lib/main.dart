e7163d53b745a932f8a64fba163d6247dd933972
Saim Khalid <khalidvr693@gmail.com>
2025-04-08 20:36:07 +0500

    new commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
+import 'firebase_options.dart';
 import 'widgets/home.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -19,3 +25,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+//saim
